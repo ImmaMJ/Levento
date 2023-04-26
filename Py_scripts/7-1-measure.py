bdbd0bcd585489ee7f18d04079438c0572078d64
ImmaMJ <112131925+ImmaMJ@users.noreply.github.com>
2023-04-26 15:27:12 +0300

    Add files via upload

---
+++ Py_scripts/7-1-measure.py
@@ -0,0 +1,75 @@
+import RPi.GPIO as GPIO
+import matplotlib.pyplot as plt
+import time
+
+
+def Dec2Bin(num):
+    return [int(el) for el in bin(int(num))[2:].zfill(8)]
+
+def num2dac(value):
+    signal = Dec2Bin(value)
+    GPIO.output(dac, signal)
+    return signal
+
+def adc():
+    level = 0
+    for i in range(bits - 1, -1, -1):
+        level += 2**i
+        GPIO.output(dac, Dec2Bin(level))
+        time.sleep(0.009)
+        comp_val = GPIO.input(comp)
+        if (comp_val == 0):
+            level -= 2**i
+    return level
+
+
+dac = [26, 19, 13, 6, 5, 11, 9, 10]
+leds   = [21, 20, 16, 12, 7, 8, 25, 24]
+comp   = 4
+troyka = 17
+bits   = len(dac)
+levels = 2 ** bits
+max_voltage = 3.3
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(dac, GPIO.OUT)
+GPIO.setup(troyka, GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(comp, GPIO.IN)
+
+GPIO.output(troyka, 0)
+
+try:
+    data = []
+    start_time = time.time()
+
+    GPIO.output(troyka, GPIO.LOW)
+    while True:
+        value = 3.3*adc()/256
+        data.append(value)
+        print(value)
+        if value >= 0.97 * 3.3:
+            break
+
+
+    GPIO.output(troyka, GPIO.HIGH)
+    while True:
+        value = 3.3*adc()/256
+        data.append(value)
+        print(value)
+        if value <= 0.02 * 3.3:
+            break
+
+
+
+
+    plt.plot(data)
+    plt.show()
+
+
+    with open('data.txt', 'w') as f:
+        for value in data:
+            f.write(str(value) + '\n')
+
+finally:
+    GPIO.cleanup()
